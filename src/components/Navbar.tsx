34ac616753df4708b5e6a80dab1d488ddb11c5ee
Ryan Stewart <ryanstew17@gmail.com>
2024-03-23 20:02:03 -0300

    store page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,28 @@
-import { Container, Nav, Navbar as NavbarBs } from "react-bootstrap"
-import { NavLink } from "react-router-dom"
+import { Container, Nav, Navbar as NavbarBs } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
 export function Navbar() {
   return (
     <NavbarBs sticky="top" className="bg-white shadow-sm mb-3"> 
-   
-  <Container>
-    <Nav className="me-auto">
-<Nav.Link to="/" as={NavLink}>
-  Home
-</Nav.Link>
-<Nav.Link to="/store" as={NavLink}>
-  Store
-</Nav.Link>
-<Nav.Link to="/about" as={NavLink}>
-  About
-</Nav.Link>
-    </Nav>
-    <button>
-    <img src="/cart-icon.png" alt="Cart" style={{ width: '50px', height: '50px' }} />
-     </button>
-     <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center" style={{ color: "white", width: "1.5rem", height: "1.5rem", position: "absolute", bottom: 4, right: 120, transform: "translate(25%, 25%)"}}>3</div>
-  </Container>
-  </NavbarBs>
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <div style={{ display: 'flex', alignItems: 'center', position: 'relative' }}>
+          <button>
+            <img src="/cart-icon.png" alt="Cart" style={{ width: '50px', height: '50px' }} />
+          </button>
+          <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center" style={{ color: "white", width: "1.5rem", height: "1.5rem", position: "absolute", bottom: '4%', right: '5%'}}>3</div>
+        </div>
+      </Container>
+    </NavbarBs>
   )
 }
